be7335ab9b7ca09ae0550158c2167e9b5d795cc4
hyejin <gowls203@naver.com>
2025-03-10 22:18:40 +0900

    삭제

--- main.py
+++
@@ -1,8 +0,0 @@
-try:
-    file = open('mission_computer_main.log', 'r', encoding = 'utf-8')
-    log = file.read()
-    file.close()
-    print(log)
-
-except FileNotFoundError:
-    print('파일이 존재햐지 않습니다.')
