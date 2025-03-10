9e24b57183b17d5f9a2c76e55bbb65c84023ce6b
hyejin <gowls203@naver.com>
2025-03-10 17:18:14 +0900

    수정정

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello Mars')
+try:
+    file = open('mission_computer_main.log', 'r', encoding = 'utf-8')
+    log = file.read()
+    file.close()
+    print(log)
+
+except FileNotFoundError:
+    print('파일이 존재햐지 않습니다.')
